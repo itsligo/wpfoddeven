6e0e0762b6118fe8978e317a7dfc2f8d3989cdd4
John Kelleher <kelleher.john@itsligo.ie>
2015-09-26 20:28:36 +0100

    first commit

---
+++ wpfOddEvenGame/ColorValueConverter.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows.Data;
+using System.Windows.Media;
+
+namespace wpfOddEvenGame
+{
+    public class SliderValueConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            int sldValue = Int32.Parse(value.ToString());
+            if (sldValue<3) return new SolidColorBrush(Colors.AntiqueWhite);
+            if (sldValue<6) return new SolidColorBrush(Colors.Beige);
+            if (sldValue < 8) return new SolidColorBrush(Colors.BurlyWood);
+            if (sldValue<11) return new SolidColorBrush(Colors.DarkCyan);
+            return new SolidColorBrush(Colors.DarkCyan);
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ wpfOddEvenGame/wpfOddEven.xaml.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Threading;
+using System.Diagnostics;
+using System.Windows.Controls;
+
+namespace wpfOddEvenGame
+{
+    /// <summary>
+    /// Interaction logic for Window1.xaml
+    /// </summary>
+    public partial class WPFWindow : Window
+    {
+        DispatcherTimer dispatcherTimer = new DispatcherTimer();    // declare and create the timer
+        Random rnd;         // declare the random number generator
+        int noOdd = 0, noEven = 0;  // declare and initialise the counters
+
+        public WPFWindow()
+        {
+            InitializeComponent();
+            //int seed = Convert.ToInt32(DateTime.Now.Millisecond); // alternative to seed the random number generator
+            int seed = System.Environment.TickCount;        // seed for randomisation
+            rnd = new Random(seed);
+        }
+
+        private void dispatcherTimer_Tick (object sender, EventArgs e) {
+            int test = rnd.Next(100);
+            if ((test % 2) == 0) noEven++; else noOdd++;
+            txtRandom.Text = test.ToString();
+            txtResult.Text = String.Format("Odd: {0}\r\nEven: {1}", noOdd, noEven);
+        }
+
+        private void btnStart_Click(object sender, RoutedEventArgs e)
+        {
+            // wire up the event handler for the 'Tick' event
+            dispatcherTimer.Tick += new EventHandler(dispatcherTimer_Tick);
+            // set the timer to 'tick' every 1second
+            dispatcherTimer.Interval = new TimeSpan(0, 0, 0, 0, (int)(1000/sldRate.Value));
+            // start the timer going
+            dispatcherTimer.Start();
+
+            noOdd = noEven = 0;         // zero the counters
+            btnStart.IsEnabled = false; // reflect changes to UI
+            btnStop.IsEnabled = true;
+        }
+
+        private void btnStop_Click(object sender, RoutedEventArgs e)
+        {
+            btnStart.IsEnabled = true;  // reflect changes to UI
+            btnStop.IsEnabled = false;
+            dispatcherTimer.Stop();     // stop the timer
+        }
+
+        private void sldRate_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            // slider ranges from 1 - 10
+            // timer should range 1000 - 100
+            //txtSlider.Text = sldRate.Value.ToString();
+            //txtSlider.Text = ((Slider)sender).Value.ToString();
+            //txtSlider.Text = (sender as Slider).Value.ToString();
+            dispatcherTimer.Interval = new TimeSpan(0, 0, 0,0,(int)(1000/sldRate.Value));
+        }
+    }   // end class
+}   // end namespace
